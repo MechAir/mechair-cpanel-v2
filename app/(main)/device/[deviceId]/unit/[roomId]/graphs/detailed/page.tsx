348a2f8f72addacaace0b69f20c8ef5c938b99c0
DhyeyBhavsar <165992161+DhyeyBhavsar@users.noreply.github.com>
2026-06-09 02:54:04 +0530

    Create page.tsx

---
+++ app/(main)/device/[deviceId]/unit/[roomId]/graphs/detailed/page.tsx
@@ -0,0 +1,3 @@
+'use client'
+import RoomDetailedGraphsPage from '../../../../room/[roomId]/graphs/detailed/page'
+export default function UnitDetailedGraphsPage() { return <RoomDetailedGraphsPage /> }
